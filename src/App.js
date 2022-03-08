51e0426cd8f85516e681bee86ae8543ac5d406c6
jordi3d <jordi@cine3d.com>
2022-03-08 17:24:42 +0100

    simplificant l App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import { useState } from "react";
 import "./App.css";
+import { HashRouter, Routes /*Route*/ } from "react-router-dom";
+/*import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import NoPage from "./pages/NoPage";*/
 import Linies from "./Linies";
 import Caixes from "./Caixes";
 
 export default function App() {
   const [draw, setDraw] = useState(false);
   return (
-    <div className="App">
-      <Linies draw={draw} />
-      <Caixes setDraw={setDraw} />
-    </div>
+    <HashRouter>
+      <Routes>
+        {/*<Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />*/}
+        <div className="App">
+          <Linies draw={draw} />
+          <Caixes setDraw={setDraw} />
+        </div>
+        {/*<Route path="*" element={<NoPage />} />
+        </Route>*/}
+      </Routes>
+    </HashRouter>
   );
 }
